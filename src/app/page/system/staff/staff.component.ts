9b32112fed387fb94d7e1eb665805a9f7a258acf
Sirius.Black <ofghrt@qq.com>
2020-03-30 15:41:56 +0800

    style: code format

--- src/app/page/system/staff/staff.component.ts
+++ src/app/page/system/staff/staff.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MsgService } from '../../../service/msg/msg.service';
-import { StaffService } from '../../../service/system/staff.service';
-import { QueryModel } from './query.model';
-import { ENUM, RESPONSE } from '../../../models';
-import { AdaptorUtils, ObjectUtils } from '@shared/utils';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Service } from '../../../../decorators/service.decorator';
-import { DepartService, RoleService, ShopService, SysMenuService } from '../../../service/system';
-import { NzTreeComponent } from 'ng-zorro-antd';
-import { Before, CombineAll } from '../../../../decorators/function.decorator';
-import { Observable } from 'rxjs';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MsgService} from '../../../service/msg/msg.service';
+import {StaffService} from '../../../service/system/staff.service';
+import {QueryModel} from './query.model';
+import {ENUM, RESPONSE} from '../../../models';
+import {AdaptorUtils, ObjectUtils} from '@shared/utils';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Service} from '../../../../decorators/service.decorator';
+import {DepartService, RoleService, ShopService, SysMenuService} from '../../../service/system';
+import {NzTreeComponent} from 'ng-zorro-antd';
+import {Before, CombineAll} from '../../../../decorators/function.decorator';
+import {Observable} from 'rxjs';
 import * as md5 from 'md5' ;
+
 @Component({
 	selector: 'sys-staff',
 	templateUrl: './staff.component.html',
@@ -25,40 +26,41 @@ export class StaffComponent implements OnInit {
 		private roleSer: RoleService,
 		private fb: FormBuilder,
 		private shopSer: ShopService
-	) {} ;
+	) {
+	} ;
 
 	ngOnInit(): void {
 		this.getRoleList();
 		this.getDepart();
 		this.getStaffList();
-		this.getShops() ;
+		this.getShops();
 	};
 
 	isVisible: boolean = false;
 	queryModel: QueryModel = new QueryModel;
 	editMark: boolean = false;
 	formShow: boolean = false;
-	passFormShow: boolean = false ;
+	passFormShow: boolean = false;
 
 	form: FormGroup = this.fb.group({
 		'username': [null, [Validators.required]],
 		'name': [null, [Validators.required]],
-		'remark': [ null ],
+		'remark': [null],
 		'phoneNumber': [null],
 		'password': [null],
 		'roleIds': [null, [Validators.required]],
-		'shopId' : [null , [Validators.required ]] ,
+		'shopId': [null, [Validators.required]],
 		'id': [null],
 	});
 
 	passForm: FormGroup = this.fb.group({
-		'password' : [null , [Validators.required]] ,
-		'passwordAgain' : [null , [Validators.required]] ,
-		'id': [ null ]
+		'password': [null, [Validators.required]],
+		'passwordAgain': [null, [Validators.required]],
+		'id': [null]
 	});
 
 	roles: ENUM[] = [];
-	ENUM_Shops: ENUM[] ;
+	ENUM_Shops: ENUM[];
 
 	departs: any[] = [];
 	selectDepart: string[];
@@ -69,39 +71,48 @@ export class StaffComponent implements OnInit {
 		page: 1,
 		total: 0,
 		columns: [
-			{ title: '用户名', type: 'text', reflect: 'username' },
-			{ title: '手机号', type: 'text', filter : ( val ) => {
-				return val.phoneNumber ? val.phoneNumber : "未填写" ;
-			}},
-			{ title: '真实姓名', type: 'text', reflect: 'name' },
-			{ title: '部门' , type: 'text' , filter : ( val ) => {
-				let str = '' ;
-				val.departmentDTOS.forEach( item => {
-					str += item.name + "," ;
-				});
-
-				return str ;
-			}},
-			{ title: '所属店' , type: 'text' , filter : ( val ) => {
-					let str = '' ;
-					val.shopOutputVOS.forEach( item => {
-						str += item.name + "," ;
+			{title: '用户名', type: 'text', reflect: 'username'},
+			{
+				title: '手机号', type: 'text', filter: (val) => {
+					return val.phoneNumber ? val.phoneNumber : "未填写";
+				}
+			},
+			{title: '真实姓名', type: 'text', reflect: 'name'},
+			{
+				title: '部门', type: 'text', filter: (val) => {
+					let str = '';
+					val.departmentDTOS.forEach(item => {
+						str += item.name + ",";
 					});
 
-					return str ;
-				}},
-			{ title: '角色' , type: 'text' , filter : ( val ) => {
-				let str = '' ;
-				val.roleOutputVOS.forEach( item => {
-					str += item.name + "," ;
-				});
+					return str;
+				}
+			},
+			{
+				title: '所属店', type: 'text', filter: (val) => {
+					let str = '';
+					val.shopOutputVOS.forEach(item => {
+						str += item.name + ",";
+					});
 
-				return str ;
-			}},
-			{ title: '冻结', type: 'switch', filter: (column) => {
+					return str;
+				}
+			},
+			{
+				title: '角色', type: 'text', filter: (val) => {
+					let str = '';
+					val.roleOutputVOS.forEach(item => {
+						str += item.name + ",";
+					});
+
+					return str;
+				}
+			},
+			{
+				title: '冻结', type: 'switch', filter: (column) => {
 					return column.status === 1;
 				}, fn: (data) => {
-					this.changeStatus( data );
+					this.changeStatus(data);
 				},
 			}
 		],
@@ -109,29 +120,29 @@ export class StaffComponent implements OnInit {
 		btn: {
 			title: '操作',
 			items: [{
-					type: 'del',
-					title: '删除',
-					fn: (data) => {
-						this.isVisible = true;
-						this.form.patchValue(data);
-					},
-				},{
-					type: 'edit',
-					title: '编辑',
-					fn: (data) => {
-						this.editMark = true;
-						this.editShow(data);
-					},
-				},{
-					type: 'reset',
-					title: '修改密码',
-					fn: ( data ) => {
-						this.passForm.patchValue({
-							id: data.id
-						});
-						this.passFormShow = true ;
-					},
+				type: 'del',
+				title: '删除',
+				fn: (data) => {
+					this.isVisible = true;
+					this.form.patchValue(data);
 				},
+			}, {
+				type: 'edit',
+				title: '编辑',
+				fn: (data) => {
+					this.editMark = true;
+					this.editShow(data);
+				},
+			}, {
+				type: 'reset',
+				title: '修改密码',
+				fn: (data) => {
+					this.passForm.patchValue({
+						id: data.id
+					});
+					this.passFormShow = true;
+				},
+			},
 			],
 		},
 		change: (type: string, size: number) => {
@@ -147,9 +158,9 @@ export class StaffComponent implements OnInit {
 
 	searchBarData = {
 		conditions: [
-			{ name: '用户名', type: 'input', model: 'username', placeHolder: '请输入用户名' },
-			{ name: '手机号', type: 'input', model: 'phoneNumber', placeHolder: '请输入手机号' },
-			{ name: '真实姓名', type: 'input', model: 'description', placeHolder: '请输入真实姓名' }
+			{name: '用户名', type: 'input', model: 'username', placeHolder: '请输入用户名'},
+			{name: '手机号', type: 'input', model: 'phoneNumber', placeHolder: '请输入手机号'},
+			{name: '真实姓名', type: 'input', model: 'description', placeHolder: '请输入真实姓名'}
 		],
 		notify: {
 			query: (data: QueryModel) => {
@@ -164,16 +175,16 @@ export class StaffComponent implements OnInit {
 	};
 
 	getStaffList() {
-		this.tableData.loading = true ;
+		this.tableData.loading = true;
 		this.service.getList(this.queryModel)
 			.subscribe((res: RESPONSE) => {
-				this.tableData.data = res.data ;
+				this.tableData.data = res.data;
 				this.tableData.loading = false;
-				if(res.page)
-					this.tableData.total = res.page.totalNumber ;
-			},err => {
-				this.tableData.loading = false ;
-				this.msg.error( err ) ;
+				if (res.page)
+					this.tableData.total = res.page.totalNumber;
+			}, err => {
+				this.tableData.loading = false;
+				this.msg.error(err);
 			});
 	};
 
@@ -184,7 +195,7 @@ export class StaffComponent implements OnInit {
 		this.form.reset();
 	};
 
-	@Service('service.delete', true, function() {
+	@Service('service.delete', true, function () {
 		return this.form.value;
 	})
 	modalConfirm($event: MouseEvent) {
@@ -193,7 +204,7 @@ export class StaffComponent implements OnInit {
 		this.getStaffList();
 	};
 
-	@Before(function($event: Event) {
+	@Before(function ($event: Event) {
 		const selectKeys = (this as StaffComponent).treeCom.nzTreeService.getSelectedNodeList();
 
 		if (selectKeys.length === 0) {
@@ -218,7 +229,7 @@ export class StaffComponent implements OnInit {
 	makeNew($event: Event, selectKeys: string[]): void {
 		const value = this.form.value;
 		value['departIds'] = selectKeys;
-		value.password =  md5(value.password) ;
+		value.password = md5(value.password);
 		this.service.post(value)
 			.subscribe((res: RESPONSE) => {
 				this.msg.notifySuccess('新建管理员成功', `用户名为: ${value.username} , 密码:${value.password}`);
@@ -227,7 +238,7 @@ export class StaffComponent implements OnInit {
 			});
 	};
 
-	@Before(function($event: Event) {
+	@Before(function ($event: Event) {
 		const selectKeys = (this as StaffComponent).treeCom.nzTreeService.getSelectedNodeList();
 		if (selectKeys.length === 0) {
 			(this as StaffComponent).msg.warn('未选择部门');
@@ -247,19 +258,18 @@ export class StaffComponent implements OnInit {
 			"username": value.username,
 			"name": value.name,
 			"remark": value.remark,
-			"phoneNumber": value.phoneNumber ,
-			"password": value.password ? md5(value.password) : md5('123123'),
+			"phoneNumber": value.phoneNumber,
 			"roleIds": value.roleIds,
 			"shopId": value.shopId,
-			"id": value.id ,
+			"id": value.id,
 			"departIds": selectKeys
 		};
-
+		if( value.password ) data['password'] = md5( value.password ) ;
 		this.service.put(data)
 			.subscribe((res: RESPONSE) => {
 				this.msg.success('修改成功');
 				this.formShow = false;
-				this.getStaffList() ;
+				this.getStaffList();
 			});
 	};
 
@@ -278,7 +288,7 @@ export class StaffComponent implements OnInit {
 	getDepart(): void {
 		this.departSer.get()
 			.subscribe((res: RESPONSE) => {
-				this.departs = AdaptorUtils.makeTreeNode({ title: 'name', key: 'id' }, res.data);
+				this.departs = AdaptorUtils.makeTreeNode({title: 'name', key: 'id'}, res.data);
 			});
 	};
 
@@ -287,22 +297,22 @@ export class StaffComponent implements OnInit {
 
 		let departs = [];
 
-		let shop = [] ;
+		let shop = [];
 
 		data.roleOutputVOS.forEach(item => {
 			roles.push(item.id);
 		});
 
 		data.departmentDTOS.forEach(item => {
-			departs.push( item.id );
+			departs.push(item.id);
 		});
 
 		data.shopOutputVOS.forEach(item => {
-			shop.push( item.id );
+			shop.push(item.id);
 		});
 
 		data.roleIds = roles;
-		data.shopId = shop ;
+		data.shopId = shop;
 		this.selectDepart = departs;
 
 		this.form.patchValue(data);
@@ -310,45 +320,45 @@ export class StaffComponent implements OnInit {
 		this.formShow = true;
 	};
 
-	@Service("service.put", true , function(){
-		const formValue = (this as StaffComponent).passForm.value ;
+	@Service("service.put", true, function () {
+		const formValue = (this as StaffComponent).passForm.value;
 		return {
 			id: formValue.id,
 			password: formValue.password
-		} ;
+		};
 	})
-	changePass( $event: MouseEvent): void{
-		this.msg.success("修改成功") ;
-		this.passFormShow = false ;
-		this.passForm.reset() ;
+	changePass($event: MouseEvent): void {
+		this.msg.success("修改成功");
+		this.passFormShow = false;
+		this.passForm.reset();
 	}
 
-	changeStatus(data : any ) {
+	changeStatus(data: any) {
 		const postData = {
-			id :  data.id ,
-			status : data.status === 1 ? 0 : 1
+			id: data.id,
+			status: data.status === 1 ? 0 : 1
 		};
 
 		this.service.put(postData)
-			.subscribe( ( res ) => {
-				data.status === 1 ? data.status = 0 : data.status = 1 ;
-				this.msg.success("修改成功") ;
+			.subscribe((res) => {
+				data.status === 1 ? data.status = 0 : data.status = 1;
+				this.msg.success("修改成功");
 			})
 	};
 
-	getShops(): void{
+	getShops(): void {
 		this.shopSer.getAll()
-			.subscribe( ( res : RESPONSE ) => {
-				this.ENUM_Shops = AdaptorUtils.reflect( res.data , {'name': 'key' , id: 'value' })
+			.subscribe((res: RESPONSE) => {
+				this.ENUM_Shops = AdaptorUtils.reflect(res.data, {'name': 'key', id: 'value'})
 			});
 	};
 
 }
 
-const selectId = function(arr: any[], tar: string[]) {
+const selectId = function (arr: any[], tar: string[]) {
 	arr.forEach(item => {
 		tar.push(item.key.toString());
-			if (item.children.length > 0) {
+		if (item.children.length > 0) {
 			selectId(item.children, tar);
 		}
 	});
